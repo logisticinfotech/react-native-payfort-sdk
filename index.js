7aa195887e0ea5ed6a8d0d0d133b09b87d8c22b3
Naishadh Sakariya <naishadh@logisticinfotech.co.in>
2019-12-27 18:31:37 +0530

    commit misc changes

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 
-import { NativeModules } from 'react-native';
 import PayFortSDK from './PayFortSDK/PayFortSDK'
 
-const { RNPayfortSdk } = NativeModules;
-
-// export default RNPayfortSdk;
 export default PayFortSDK;
