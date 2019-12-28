a211753076fc191a7a6faa589ae2d76b744507f5
Bhavin Rajpara <bhavinrajpara@logisticinfotech.co.in>
2019-12-28 11:11:38 +0530

    ios  code update

---
+++ ios/RNPayfortSdk-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+#import <React/RCTBridgeModule.h>
+#import <React/RCTViewManager.h>
+#import <React/RCTBridge.h>
+#import <React/RCTUIManager.h>
+#ifndef PayFortDemo_Bridging_Header_h
+#define PayFortDemo_Bridging_Header_h
+#import <PayFortSDK/PayFortView.h>
+#import <PayFortSDK/PayFortSDK.h>
+#import <CommonCrypto/CommonHMAC.h>
+
+#endif /* PayFortDemo_Bridging_Header_h */
