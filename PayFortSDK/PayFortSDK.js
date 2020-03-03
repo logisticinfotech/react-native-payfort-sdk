3ccd9965ac25443998a2e2fa6e01bddabd6a98b2
Shubham <shubham@logisticinfotech.co.in>
2020-03-03 12:42:40 +0530

    Android Permission issue solve and update version

--- PayFortSDK/PayFortSDK.js
+++ PayFortSDK/PayFortSDK.js
@@ -7,46 +7,87 @@
  */
 
 // import payFort from './src/Component/PayFort/PayFort';
-import {NativeModules, Platform} from 'react-native';
-const {PayFort} = NativeModules;
+import { NativeModules, Platform, PermissionsAndroid } from "react-native";
+const { PayFort } = NativeModules;
 
 const RNPayFort = async parameter => {
-  return new Promise(async (resolve, reject) => {
-    if (
-      parameter.command &&
-      parameter.access_code &&
-      parameter.merchant_identifier &&
-      parameter.sha_request_phrase &&
-      parameter.email &&
-      parameter.language &&
-      parameter.amount &&
-      parameter.currencyType &&
-      parameter.testing
-    ) {
-      await PayFort.Pay(
-        JSON.stringify(parameter),
-        successResponseData => {
-          resolve(
-            Platform.OS === 'android'
-              ? JSON.parse(successResponseData)
-              : successResponseData,
-          );
-        },
-        errorResponseData => {
-          reject(
-            Platform.OS === 'android'
-              ? JSON.parse(errorResponseData)
-              : errorResponseData,
-          );
-        },
+  if (Platform.OS === "android") {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE
       );
-    } else {
-      reject({
-        response_code: 'MissingParameter',
-        response_message: 'Please enter all required Parameter.',
-      });
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        return new Promise(async (resolve, reject) => {
+          if (
+            parameter.command &&
+            parameter.access_code &&
+            parameter.merchant_identifier &&
+            parameter.sha_request_phrase &&
+            parameter.email &&
+            parameter.language &&
+            parameter.amount &&
+            parameter.currencyType &&
+            parameter.testing
+          ) {
+            await PayFort.Pay(
+              JSON.stringify(parameter),
+              successResponseData => {
+                resolve(JSON.parse(successResponseData));
+              },
+              errorResponseData => {
+                reject(JSON.parse(errorResponseData));
+              }
+            );
+          } else {
+            reject({
+              response_code: "MissingParameter",
+              response_message: "Please enter all required Parameter."
+            });
+          }
+        });
+      } else {
+        console.log("Read phone state permission denied");
+        return new Promise(async (resolve, reject) => {
+          reject({
+            response_code: "PermissionRequired",
+            response_message: "Grant permission for Read phone state."
+          });
+        });
+      }
+    } catch (err) {
+      console.warn(err);
     }
-  });
+  }
+  if (Platform.OS === "ios") {
+    return new Promise(async (resolve, reject) => {
+      if (
+        parameter.command &&
+        parameter.access_code &&
+        parameter.merchant_identifier &&
+        parameter.sha_request_phrase &&
+        parameter.email &&
+        parameter.language &&
+        parameter.amount &&
+        parameter.currencyType &&
+        parameter.testing
+      ) {
+        await PayFort.Pay(
+          JSON.stringify(parameter),
+          successResponseData => {
+            resolve(successResponseData);
+          },
+          errorResponseData => {
+            reject(errorResponseData);
+          }
+        );
+      } else {
+        reject({
+          response_code: "MissingParameter",
+          response_message: "Please enter all required Parameter."
+        });
+      }
+    });
+  }
 };
 
 export default RNPayFort;
